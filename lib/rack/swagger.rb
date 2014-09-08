69f17292c88594c2f50a595e00dd68af4604ceb5
Colin Rymer <colin.rymer@gmail.com>
2014-09-08 11:23:13 -0400

    initial commit

---
+++ lib/rack/swagger.rb
@@ -0,0 +1,7 @@
+require "rack/swagger/version"
+
+module Rack
+  module Swagger
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/swagger/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Swagger
+    VERSION = "0.0.1"
+  end
+end
